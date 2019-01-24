91e01a3cad864968524060dd8d6d63eda251fc6f
Aszmel <aszmel@o2.pl>
2019-01-24 19:01:09 +0100

    first and last commit :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-class App extends Component {
+const MyContext = React.createContext();
+
+class MyProvider extends Component {
+  state = {
+    name: "Aszmel",
+    age: 100,
+    cool: true
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growOneYear: () => this.setState({ age: this.state.age + 1 }),
+          downOneYear: () => this.setState({ age: this.state.age - 1 })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <>
+              <p>Name: {context.state.name}</p>
+              <p>Age: {context.state.age}</p>
+              <button onClick={context.growOneYear}>+1 Year</button>
+              <button onClick={context.downOneYear}>-1 Year</button>
+            </>
+          )}
+        </MyContext.Consumer>
       </div>
+    );
+  }
+}
+
+const Family = props => {
+  return (
+    <div className="family">
+      <Person />
+    </div>
+  );
+};
+
+class App extends Component {
+  state = {
+    name: "Aszmel",
+    age: 100,
+    cool: true
+  };
+  render() {
+    return (
+      <MyProvider>
+        <div className="App">
+          <p>I'm the app</p>
+          <Family name={this.state.name} />
+        </div>
+      </MyProvider>
     );
   }
 }
